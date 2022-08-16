5866c4ab9b372c3f3621ecc03270729b45b2739e
Fenglin Wu <quic_fenglinw@quicinc.com>
2022-08-16 13:25:54 +0800

    healthd-ext: replace to use AIDL implementation
    Change it back to use AIDL health HAL implementation as the dependent
    VSDK issue has been addressed.
    
    Change-Id: Ie9d08b6397216bd4dc4ecc7ef8acdf3ba46ffd4f

--- health-vendor-product.mk
+++ health-vendor-product.mk
@@ -1,2 +1,3 @@
-PRODUCT_PACKAGES += android.hardware.health@2.1-impl-qti
-PRODUCT_PACKAGES += android.hardware.health@2.1-service
+PRODUCT_PACKAGES += android.hardware.health-service.qti
+PRODUCT_PACKAGES += android.hardware.health-service.qti_recovery
+PRODUCT_PACKAGES += charger_res_images_vendor
