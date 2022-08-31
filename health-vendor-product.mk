0bdc4935b56af8183fb003421f75ab8f937e2f4b
Shashank Sivakumar <quic_shassiva@quicinc.com>
2022-08-31 08:48:15 -0700

    Revert "healthd-ext: replace to use AIDL implementation"
    This reverts commit 5866c4ab9b372c3f3621ecc03270729b45b2739e.
    
    Change-Id: Id3030ee85088b3ffeb8e69aec7a3fc45fe7742d5

--- health-vendor-product.mk
+++ health-vendor-product.mk
@@ -1,3 +1,2 @@
-PRODUCT_PACKAGES += android.hardware.health-service.qti
-PRODUCT_PACKAGES += android.hardware.health-service.qti_recovery
-PRODUCT_PACKAGES += charger_res_images_vendor
+PRODUCT_PACKAGES += android.hardware.health@2.1-impl-qti
+PRODUCT_PACKAGES += android.hardware.health@2.1-service
